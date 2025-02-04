447178fc3ee23d5f5be4db90e7a1ef9e76c1f374
Mathbkj <matheusgblasel@hotmail.com>
2025-02-04 16:37:39 -0300

    Framer Motion Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 import Image from "./assets/recipe-page-main/assets/images/image-omelette.jpeg";
 type PreparationType = {
   name?: string;
@@ -49,51 +51,97 @@ function App() {
       value: "Serve hot, with additional salt and pepper if needed.",
     },
   ];
+  const constraintRef = useRef<HTMLDivElement>(null);
+  const constraints = {
+    width: "50%",
+    borderRadius: 10,
+  };
+  const offScreen = {
+    x: -10,
+    opacity: 0,
+  };
+  const onScreen = {
+    x: 20,
+    y:10,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.4,
+      duration: 0.8,
+    },
+  };
+  const { scrollY } = useScroll();
+  const rotate = useTransform(scrollY, [0, 1000], [0, -30]);
+
   return (
-    <main className="flex justify-center items-center text-center bg-white">
-      <div className="flex flex-col gap-2 justify-center items-center mt-4">
-        <img
+    <main className="flex justify-center items-center text-center ">
+      <div className="flex flex-col gap-2 justify-center items-center w-1/2 mt-4 py-4 rounded-md bg-white">
+        <motion.img
           width={500}
           height={500}
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: "cover", borderRadius: 10,rotate:rotate }}
           src={Image}
         />
-        <h1 className="text-2xl font-young-serif">Simple Omelette Recipe</h1>
+        <motion.h1
+          initial={{ opacity: 0, x: 0 }}
+          whileInView={{ opacity: 1, x: 1, transition: { duration: 0.8 } }}
+          className="text-2xl font-young-serif"
+        >
+          Simple Omelette Recipe
+        </motion.h1>
         <br />
-        <p className="text-xl font-outfit-light ">
+        <motion.p
+          initial={{ opacity: 0, x: 0 }}
+          whileInView={{ opacity: 1, x: 1, transition: { duration: 0.8 } }}
+          className="text-xl font-outfit-light "
+        >
           An easy and quick dish, perfect for any meal. This classic omelette
           combines beaten eggs cooked to perfection, optionally filled with your
           choice of cheese, vegetables, or meats.
-        </p>
+        </motion.p>
         <br />
-        <div className="time-div">
-          <h1 className="font-outfit-bold ms-4 mt-5 text-xl">
-            Preparation Time
-          </h1>
-          <ol className="list-disc list-inside">
-            {timeItems.map((item) => (
-              <li className="text-left m-4">
-                <span className="font-outfit-medium text-left">
-                  {item.name}:{" "}
-                </span>
-                <span className="font-outfit-light text-left">
-                  {item.value}
-                </span>
-                <br />
-              </li>
-            ))}
-          </ol>
-        </div>
-        <div className="ing-div">
+        <motion.div ref={constraintRef} style={constraints}>
+          <motion.div
+            initial={{ outlineWidth: 0 }}
+            whileHover={{ scale: 1.2 }}
+            drag
+            dragConstraints={constraintRef}
+            dragElastic={0.8}
+            className="time-div"
+          >
+            <h1 className="font-outfit-bold ms-4 mt-5 text-xl">
+              Preparation Time
+            </h1>
+            <ol className="list-disc list-inside">
+              {timeItems.map((item) => (
+                <li className="text-left m-4">
+                  <span className="font-outfit-medium text-left">
+                    {item.name}:{" "}
+                  </span>
+                  <span className="font-outfit-light text-left">
+                    {item.value}
+                  </span>
+                  <br />
+                </li>
+              ))}
+            </ol>
+          </motion.div>
+        </motion.div>
+
+        <motion.div
+          initial={offScreen}
+          whileInView={onScreen}
+          className="ing-div"
+        >
           <h1 className="font-young-serif ms-4 mt-5 text-3xl">Ingredients</h1>
           <ol className="list-disc list-inside">
             {ingItems.map((item) => (
               <li className="text-left m-4">{item.value}</li>
             ))}
           </ol>
-        </div>
+        </motion.div>
         <hr className="w-[70%]" />
-        <div className="instr-div">
+        <motion.div initial={offScreen} whileInView={onScreen} className="instr-div">
           <h1 className="font-young-serif ms-4 mt-5 text-3xl">Instructions</h1>
           <ol className="list-decimal list-inside">
             {instrItems.map((item) => (
@@ -108,9 +156,9 @@ function App() {
               </li>
             ))}
           </ol>
-        </div>
+        </motion.div>
         <hr className="w-[70%]" />
-        <div className="nutr-div p-2">
+        <motion.div initial={offScreen} whileInView={onScreen} className="nutr-div p-2">
           <h1 className="font-young-serif ms-4 mt-5 text-3xl">Nutrition</h1>
           <p className="font-outfit-light">
             The table below shows nutritional values per serving without the
@@ -135,7 +183,7 @@ function App() {
             </tbody>
           </table>
           <hr className="w-[90%] mx-auto" />
-        </div>
+        </motion.div>
       </div>
     </main>
   );
